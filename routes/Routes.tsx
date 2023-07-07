153b61f8e87189161f7480ab17c79d22410eb25c
Paulo <paulo.leite@academico.ifpb.edu.br>
2023-07-07 14:52:22 -0300

    initial commit

---
+++ routes/Routes.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Routes (): JSX.Element {
+  return (
+    <View>
+      <Text>Routes</Text>
+    </View>
+  )
+}
