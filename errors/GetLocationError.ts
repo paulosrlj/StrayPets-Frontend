b30f2be111ddd46b18eae59bfe64a1a18ed0d29f
Paulo <paulo.leite@academico.ifpb.edu.br>
2023-07-25 13:40:15 -0300

    feat: added error treatment

---
+++ errors/GetLocationError.ts
@@ -0,0 +1,11 @@
+export class GeolocationError extends Error {
+
+}
+
+export class GetLocationError extends GeolocationError {
+
+}
+
+export class TimetoutLocationError extends GeolocationError {
+
+}
